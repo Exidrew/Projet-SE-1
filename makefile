f203eba9e6e3e1ab7f8c7b1a5153ecb5ab91d5fe
Sebastien <sebastienduterte@hotmail.fr>
2020-12-20 16:10:00 +0100

    myssh de base (sans reseau)

--- makefile
+++ makefile
@@ -1,8 +1,20 @@
 CC = gcc
 SOURCES = error.c tinyShell.c gestionChaine.c variablesLocales.c variables.c tubeCommunication.c cd.c
 PSSOURCES = myps.c procdata.c error.c redirections.c tubeCommunication.c
+SSH_SOURCES = myssh.c error.c
 FLAGS = -Wall
 DEBUG_FLAGS = -Wall -g
+
+myssh : $(SSH_SOURCES)
+	make clear
+	$(CC) $(FLAGS) $^ -o $@
+
+myssh-debug : $(SSH_SOURCES)
+	make clear
+	$(CC) $(DEBUG_FLAGS) $^ -o $@
+
+debugmyssh :
+	valgrind --tool=memcheck --leak-check=full --leak-resolution=high --show-reachable=yes --track-origins=yes -s ./myssh-debug
 
 make ts : $(SOURCES) $(PSSOURCES)
 	make clear
@@ -29,4 +41,4 @@ debugts :
 	valgrind --tool=memcheck --leak-check=full --leak-resolution=high --show-reachable=yes --track-origins=yes -s ./ts-debug
 
 clear :
-	rm build-debug myps myps-debug ts ts-debug 2> /dev/null || true
+	rm build-debug myps myps-debug ts ts-debug myssh myssh-debug 2> /dev/null || true
---
+++ myssh.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <regex.h>
+#include <string.h>
+
+#include "headers/error.h"
+
+#define MAXIMUM 1024
+
+int checkIdentification(char* id) {
+    regex_t regex;
+    const char schema[43] = "^[a-zA-Z0-9_]+@([0-9]{1,3}.){3}[0-9]{1,3}$";
+
+    if (regcomp(&regex, schema, REG_EXTENDED)) {
+        regfree(&regex);
+        fatalsyserror(8);
+    }
+    
+    if (regexec(&regex, id, 0, NULL, 0)) {
+        printf("Invalid User/password for host1\n");
+        regfree(&regex);
+        return 0;
+    }
+    regfree(&regex);
+    return 1;
+}
+
+int checkArguments(int nbArgs) {
+    return nbArgs == 2;
+}
+
+void demanderPassword(char* password) {
+    char* message = "password: ";
+    write(STDOUT_FILENO, message, strlen(message));
+    scanf("%s", password);
+}
+
+void printUsage() {
+    printf("Usage : myssh username@host\n");
+    exit(0);
+}
+
+int main(int argc, char* argv[]) {
+    char* password = calloc(MAXIMUM, sizeof(char));
+    if (!checkArguments(argc)) printUsage();
+    if (!checkIdentification(argv[1])) printUsage();
+
+    demanderPassword(password);
+
+    free(password);
+    return 0;
+}
