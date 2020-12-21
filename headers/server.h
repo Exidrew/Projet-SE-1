1cb43b16425f03d320fceb9765a592e333b8a35d
Sebastien <sebastienduterte@hotmail.fr>
2020-12-21 13:38:03 +0100

    structure du serveur

---
+++ headers/server.h
@@ -0,0 +1,31 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define neterr_server(srv, n) server_destroy(srv),syserror(n);
+
+struct server {
+    int socket;
+    int acceptedSocket;
+    struct sockaddr_in servAddr;
+    struct sockaddr_in clientAddr;
+    socklen_t len;
+
+    ssize_t (*receive)(struct server* this, char* buff, size_t size);
+    void (*send)(struct server* this, char* message);
+    void (*bind)(struct server* this, int port);
+};
+
+typedef struct server* Server;
+
+Server createServerTCP();
+void destroyServer(Server this);
+void sendTCP(int socket, char* msg);
+ssize_t receiveTCP(int socket, char* buff, size_t size);
+
+#endif
---
+++ server.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+#include "headers/server.h"
+#include "headers/error.h"
+
+/* ============= Fonctions privées ============= */
+
+static ssize_t serverReceiveTCP(struct server* this, char* buf, size_t size) {
+    // Pas obligé de préciser le client avec qui on discute
+    // Car la connexion est établie physiquement
+    return recv(this->acceptedSocket, buf, size, 0);
+}
+
+static void serverSendTCP(struct server* this,char* msg){
+    // Pas obligé de préciser le client avec qui on discute
+    // Car connexion établie physiquement
+    if (send(this->acceptedSocket, msg, strlen(msg), 0) == ERR) {
+        neterr_server(this, SEND_ERR);
+    }
+}
+
+static void server_bind(struct server* this,int port){
+    this->servAddr.sin_family = AF_INET; // On est dans un protocole internet
+    this->servAddr.sin_addr.s_addr = INADDR_ANY; // Accepte connexion de localhost et de l'exterieur
+    this->servAddr.sin_port = htons((uint16_t) port); // Affecte le port via htons
+    // Bind le socket
+    if (bind(this->socket, (struct sockaddr *) &this->servAddr, sizeof(this->servAddr)) < 0) {
+        neterr_server(this, BINDING_ERR);
+    }
+}
+
+/* ============= Fonctions publiques ============= */
+
+Server server_create_tcp(){
+    Server server = calloc(1, sizeof(struct server));
+
+    int sfd;
+    if((sfd=socket(AF_INET,SOCK_STREAM,0))==ERR){
+        free(server);
+        syserror(SOCKET_ERR);
+    }
+
+    // TODO : Modifier les fonction send et receive
+    server->socket=sfd;
+    server->bind=&bind;
+    server->receive=&serverReceiveTCP;
+    server->send=&serverSendTCP;
+    memset(&server->servAddr, 0, sizeof(struct sockaddr_in));
+    memset(&server->clientAddr, 0, sizeof(struct sockaddr_in));
+    server->len=sizeof(struct sockaddr_in);
+    return server;
+}
+
+ssize_t receiveTCP(int socket, char *buf, size_t size) {
+    return recv(socket, buf, size, 0);
+}
+
+void sendTCP(int socket, char* msg) {
+    if (send(socket, msg, strlen(msg), 0) == ERR) {
+        syserror(SEND_ERR);
+    }
+}
+
+void destroyServer(Server this) {
+    close(this->socket);
+    free(this);
+}
